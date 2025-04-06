b922fe63b70412fdaeaef65817cedfb67e714445
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2025-04-06 05:40:07 +0300

    fix(darwin): fix remote-builder for darwin

--- profiles/common/remote-builder.nix
+++ profiles/common/remote-builder.nix
@@ -1,27 +1,44 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }: let
   username = "remote-builder";
+  groupname = "remote-builders";
+  defaultUid = 399;
+  defaultGid = 32001;
 in {
-  users = {
-    users.${username} =
-      {
-        openssh.authorizedKeys.keys = [
-          (builtins.readFile ../../secrets/ssh/remote-builder.pub)
-        ];
-      }
-      // lib.optionalAttrs (pkgs.stdenv.isLinux) {
-        # nix-darwin does not have this properties
-        isNormalUser = true;
-        group = "remote-builders";
-      };
+  users =
+    {
+      users.${username} =
+        {
+          openssh.authorizedKeys.keys = [
+            (builtins.readFile ../../secrets/ssh/remote-builder.pub)
+          ];
+        }
+        // lib.optionalAttrs (pkgs.stdenv.isLinux) {
+          group = groupname;
+          # nix-darwin does not have this properties
+          isNormalUser = true;
+        }
+        // lib.optionalAttrs (pkgs.stdenv.isDarwin) {
+          uid = lib.mkDefault defaultUid;
+          gid = lib.mkDefault config.users.groups.${groupname}.gid;
+        };
 
-    groups.remote-builders = {
-      members = [username];
+      groups.${groupname} =
+        {
+          members = [username];
+        }
+        // lib.optionalAttrs (pkgs.stdenv.isDarwin) {
+          gid = lib.mkDefault defaultGid;
+        };
+    }
+    // lib.optionalAttrs (pkgs.stdenv.isDarwin) {
+      knownUsers = [username];
+      knownGroups = [groupname];
     };
-  };
 
   nix.settings = {
     trusted-users = [username];
