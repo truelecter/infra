66a33101f19106a04194c6b985d85b2d62c775e7
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2025-05-14 03:20:46 +0300

    fix(klipper): fix firmwares and genconf

--- parts/klipper/packages/klipper-genconf.nix
+++ parts/klipper/packages/klipper-genconf.nix
@@ -3,21 +3,42 @@
   klipper,
   python3,
   gnumake,
+  stdenvNoCC,
   ...
-}:
-writeShellApplication {
-  name = "${klipper.pname}-genconf";
-  runtimeInputs = [
-    python3
-    gnumake
-  ];
-  text = ''
-    CURRENT_DIR=$(pwd)
-    CONFIG_FILE="$CURRENT_DIR/''${1:-config}"
-    TMP=$(mktemp -d)
-    make -C ${klipper.src} OUT="$TMP" KCONFIG_CONFIG="$CONFIG_FILE" menuconfig
-    rm -rf "$TMP" "$CONFIG_FILE.old"
-    printf "\nYour firmware configuration for klipper:\n\n"
-    cat "$CONFIG_FILE"
-  '';
-}
+}: let
+  s = stdenvNoCC.mkDerivation {
+    name = "${klipper.pname}-source";
+
+    src = klipper.src;
+
+    dontConfigure = true;
+
+    patchPhase = ''
+      patchShebangs .
+    '';
+
+    buildPhase = ''
+      ./scripts/find-firmware-extras.sh
+    '';
+
+    installPhase = ''
+      cp -r . $out/
+    '';
+  };
+in
+  writeShellApplication {
+    name = "${klipper.pname}-genconf";
+    runtimeInputs = [
+      python3
+      gnumake
+    ];
+    text = ''
+      CURRENT_DIR=$(pwd)
+      CONFIG_FILE="$CURRENT_DIR/''${1:-config}"
+      TMP=$(mktemp -d)
+      make -C ${s} OUT="$TMP" KCONFIG_CONFIG="$CONFIG_FILE" menuconfig
+      rm -rf "$TMP" "$CONFIG_FILE.old"
+      printf "\nYour firmware configuration for klipper:\n\n"
+      cat "$CONFIG_FILE"
+    '';
+  }
