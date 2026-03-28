0ba77e09347c753049e2c837126934c0196b3884
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2026-03-29 01:23:11 +0200

    chore(bblcam): slightly optimize

--- home/users/nixos/truelecter.nix
+++ home/users/nixos/truelecter.nix
@@ -1,22 +1,9 @@
-{
-  pkgs,
-  hmSuites,
-  sshKeys,
-  ...
-}: {
+{hmSuites, ...}: {
+  imports = [
+    ./truelecter-micro.nix
+  ];
+
   home-manager.users.truelecter = {
     imports = hmSuites.base ++ hmSuites.git;
-
-    home.stateVersion = "22.11";
-  };
-
-  programs.zsh.enable = true;
-
-  users.users.truelecter = {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    extraGroups = ["wheel" "docker"];
-
-    openssh.authorizedKeys.keys = sshKeys;
   };
 }
