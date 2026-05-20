8060929c145620219be61a595e37b306c4e19f5b
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2026-05-20 20:16:32 +0300

    chore(vzbot235): filguard -> sfs, vz hextrudort -> vz hextrudort low plus

--- parts/topology/catalog.nix
+++ parts/topology/catalog.nix
@@ -1,10 +1,16 @@
 let
   networks = {
     x290 = "10.3.0.0/24";
-    tailscale = "100.64.0.0/10";
+    tailscale = "100.67.0.0/10";
   };
 
   mkEntry = id: {
   };
+  # partToInt = i: builtins.fromJSON (builtins.elemAt parts i);
+
+  ipToInt = ip: let
+    parts = builtins.match "([[:digit:]]+)\\.([[:digit:]]+)\\.([[:digit:]]+)\\.([[:digit:]]+)" ip;
+  in
+    builtins.foldl' (acc: part: acc * 256 + (builtins.fromJSON part)) 0 parts;
 in {
 }
